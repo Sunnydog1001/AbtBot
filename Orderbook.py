1f059ab08380e341440b838dd3c9b07970cb23fe
Sunnydog1001 <sunnydog1001@gmail.com>
2019-03-22 14:37:51 +0300

    Update Orderbook.py

--- Orderbook.py
+++ Orderbook.py
@@ -60,7 +60,26 @@ class order(object):
 def add_order(order, Orderbook):
     Orderbook.add_order_to_orderbook(order)
 
-    
+
+def find_best_match(token1, token2):
+    best_bid = best_ask = 0
+    for dex in ExampleBook.DEXs:
+        if token1 in ExampleBook.DEXs[dex].tokens and token2 in ExampleBook.DEXs[dex].tokens[token1].Pairs:
+            book_bid = sorted(ExampleBook.DEXs[dex].tokens[token1].Pairs[token2].bid_book.items())
+            if book_bid[-1][0] > best_bid:
+                best_bid = book_bid[-1][0]
+        if token2 in ExampleBook.DEXs[dex].tokens and token1 in ExampleBook.DEXs[dex].tokens[token2].Pairs:
+            book_ask = sorted(ExampleBook.DEXs[dex].tokens[token2].Pairs[token1].bid_book.items())
+            if book_ask[-1][0] > best_ask:
+                best_ask = book_ask[-1][0]
+    if best_bid > best_ask:
+        k = best_bid / (1 / best_ask)
+    elif best_ask > best_bid:
+        k = best_ask / (1 / best_bid)
+    else: k = 1
+    return [best_bid, best_ask, k] #если коэффициент > 1, возможна прибыльная сделка
+
+
 ExampleBook = Orderbook()
 example = order("RadarRelay", "1218278hgihfh3uf3$ygf8e", 137.1, 10, "WETH", "DAI")
 add_order(example, ExampleBook)
@@ -77,8 +96,14 @@ for dex in ExampleBook.DEXs:
                 print(prc, ": volume:", ExampleBook.DEXs[dex].tokens[token].Pairs[pair].bid_book[prc][0], "orderIds:", ExampleBook.DEXs[dex].tokens[token].Pairs[pair].bid_book[prc][1], "\n")
 #RadarRelay :
 #WETH - DAI :
-#137.1 : volume: 11 orderIds: ['1218278hgihfh3uf3$ygf8e', '18778778hgihfh3uf3$ygf76g'] 
+#137.1 : volume: 11 orderIds: ['1218278hgihfh3uf3$ygf8e', '18778778hgihfh3uf3$ygf76g']
 
 #Bancor :
 #BTC - RVN :
-#1.131e-05 : volume: 2100 orderIds: ['838gd78fgf78egf78idfd8k0'] 
+#1.131e-05 : volume: 2100 orderIds: ['838gd78fgf78egf78idfd8k0']
+example4 = order("RadarRelay", "18778778hgihfh3uf3$ygf76j", 1000, 1, "WETH", "DAI")
+add_order(example4, ExampleBook)
+example5 = order("Bancor", "18778778hgihfh3uf3$ygf76k", 0.002, 1, "DAI", "WETH")
+add_order(example5, ExampleBook)
+print(find_best_match("WETH", "DAI"))
+#[1000, 0.002, 2.0]
